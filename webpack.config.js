70dbdd019359fc31f7a98ed3d9ba2ec6ee346099
Roberto Caroca <63612984+RobertoCaroca@users.noreply.github.com>
2023-04-01 23:08:56 -0400

    dist/style.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,32 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
+      // Process CSS files and generate CSS output file
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      // Process SCSS files and generate CSS output file
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
   },
+  plugins: [
+    // Extract CSS into a separate file
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ]
 };
+
+
