d529627634c014102175bfdbfd1e96c9f04baac6
Roberto Caroca <63612984+RobertoCaroca@users.noreply.github.com>
2023-04-01 23:51:06 -0300

    Commit node-modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 console.log('Hello, world!');
+import 'bootstrap';
+import './style.scss';
+
+console.log('Hello, world!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,16 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
