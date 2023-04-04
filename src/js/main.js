faef91061d7a5342ccef3bb76242a1a5252f27bb
Roberto Caroca <63612984+RobertoCaroca@users.noreply.github.com>
2023-04-04 01:52:58 -0400

    Simplificando 3

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-// Import bootstrap custom CSS
-import '../scss/styles.scss';
-
-// Import all Bootstrap JS
-import * as bootstrap from 'bootstrap'; 
- 
-// Import style.css
-import '../css/style.css';
